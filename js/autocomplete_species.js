8bb1956fd6a3a81922dcc3459447586e6cf808ca
verofa <veronica.farias@gmail.com>
2017-01-08 22:51:41 +0000

    HTTP/HTTPS configuration

--- js/autocomplete_species.js
+++ js/autocomplete_species.js
@@ -5,7 +5,8 @@
       	console.log(valor);
         $.ajax( {
           //url: "http://54.206.104.145:8080/species?ALL=" + valor,
-          url: "https://localhost:4443/species/search?q=" + valor,
+          //url: "https://localhost:4443/species/search?q=" + valor,
+          url: "https://vbaspecies.herokuapp.com/species/search?q=" + valor,
           contentType: "application/json; charset=utf-8",
           dataType: "json",
           success: function( data ) {
